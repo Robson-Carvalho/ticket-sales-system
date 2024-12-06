1e50911b489c7d97fef293195dbcb115dbece882
robson-carvalho <robsoncarvalho.tech@gmail.com>
2024-12-06 14:05:16 -0300

    :refactor: added improved

--- src/main/java/com/uefs/system/Main.java
+++ src/main/java/com/uefs/system/Main.java
@@ -1,3 +1,31 @@
+/*
+ * Desenvolvido utilizando o IntelliJ IDEA.
+ *
+ * Dependências utilizadas no projeto:
+ *
+ * - Gson (com.google.code.gson)               : 2.11.0
+ * - JUnit (junit)                             : 4.13.1
+ * - JavaFX (org.openjfx) - javafx-controls     : 17.0.6
+ * - JavaFX (org.openjfx) - javafx-fxml        : 17.0.6
+ * - JavaFX (org.openjfx) - javafx-web         : 17.0.6
+ * - JavaFX (org.openjfx) - javafx-swing       : 17.0.6
+ * - JavaFX (org.openjfx) - javafx-media       : 17.0.6
+ * - ControlsFX (org.controlsfx)               : 11.2.1
+ * - FormsFX (com.dlsc.formsfx)                : 11.6.0
+ * - ValidatorFX (net.synedra)                 : 0.5.0
+ * - Ikonli (org.kordamp.ikonli)               : 12.3.1
+ * - BootstrapFX (org.kordamp.bootstrapfx)     : 0.4.0
+ * - TilesFX (eu.hansolo)                      : 21.0.3
+ * - FXGL (com.github.almasb)                  : 17.3
+ * - JUnit Jupiter (org.junit.jupiter)         : 5.10.2
+ *
+ * Plugins utilizados:
+ *
+ * - Maven Compiler Plugin                    : 3.13.0
+ * - JavaFX Maven Plugin                      : 0.0.8
+ */
+
+
 package com.uefs.system;
 
 public class Main {
